a4d31d8144e7fb9fb8e2d9dd03d814d42f8a0153
chujchujowski2137 <xerosek@mail.com>
2025-08-31 02:40:38 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,4 @@
 <?php
-session_start();
-session_unset();
-session_destroy();
+// logout.php
 header("Location: index.php");
-// This is made by Magiczny_Jasiek, and only he can sell it. If you bought it from other vacban.wtf listing that was not this one: https://vacban.wtf/vacshop/78615/ then be careful using it. It is not official.
-?>
+exit;
